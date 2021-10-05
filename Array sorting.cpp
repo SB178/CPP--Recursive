4ac94757bee88ab0d12707a5d2bd0e19b2c645dc
S-B-178 <46382392+S-B-178@users.noreply.github.com>
2021-10-05 13:50:52 +0530

    Factorial

---
+++ Array sorting.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void sort(vector<int>& v,int temp)
+{
+    if(v.size()== 0 || v.back()<=temp)  //
+    {
+        v.push_back(temp);              //
+        return ;                        // base condition
+    }
+    int temp2= v.back();                //
+    v.pop_back();                       //
+    insert(v,temp);                     //hypothesis
+
+    v.push_back(temp2);                 //Induction
+}
+
+void div(vector<int>& v)
+{
+    if(v.size()==0)                     //
+    {
+        return ;                        // base condition
+    }
+    int temp = v.back();                //
+    v.pop_back();                       //
+    sort(v);                            //hypothesis
+
+    insert(v,temp);                     //Induction
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>ar(n,0);
+    for(int i=0;i<n;i++)
+        cin>>ar[i];
+    
+    sort(ar);
+    for(int i=0;i<n;i++)
+        cout<<ar[i];
+    return 0;
+}
